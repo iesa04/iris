d7bb372280966674c9a410df011cf4140c08ca22
T Iesa Mohamed <146453351+iesa04@users.noreply.github.com>
2025-05-18 19:26:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,4 @@ def index():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run()
